2b21fbc3d79cd0d638d7564e40a7094023b68026
lincaiyong <lincaiyong@bytedance.com>
2025-12-22 11:46:10 +0800

    app

--- handler/data.go
+++ handler/data.go
@@ -2,7 +2,10 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/lincaiyong/gui"
 	"github.com/lincaiyong/larkbase"
+	"net/http"
+	"time"
 )
 
 var requiredFields = []string{"id", "project", "vendor", "note"}
@@ -27,13 +30,22 @@ func Data(c *gin.Context) {
 	}
 	result := make([][]string, 0)
 	fields := append(requiredFields, conf.DataFields[name]...)
+	var mod time.Time
 	for _, record := range records {
 		row := make([]string, 0)
 		for _, field := range fields {
 			row = append(row, record.Data[field])
 		}
 		result = append(result, row)
+		if mod.Before(record.ModifiedTime) {
+			mod = record.ModifiedTime
+		}
 	}
+	if gui.IfNotModifiedSince(c, mod) {
+		c.Status(http.StatusNotModified)
+		return
+	}
+	gui.SetLastModified(c, mod, 0)
 	dataResponse(c, gin.H{
 		"fields": fields,
 		"data":   result,
