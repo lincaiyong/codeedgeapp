8afddf40a9fa6387a4ea7bd06ba26b6173c4d327
lincaiyong <lincaiyong@bytedance.com>
2025-12-25 17:04:15 +0800

    app

--- handler/cache/clean.go
+++ handler/cache/clean.go
@@ -6,7 +6,14 @@ import (
 	"github.com/lincaiyong/log"
 )
 
+var deleteFlag int
+
 func deleteEmptyDirectory(ctx context.Context) {
+	if deleteFlag > 0 {
+		deleteFlag--
+		return
+	}
+	deleteFlag = 100
 	_, _, err := common.RunCommand(ctx, cacheDir, "find", ".", "-type", "d", "-empty", "-depth", "-delete")
 	if err != nil {
 		log.WarnLog("fail to clean: %v", err)
